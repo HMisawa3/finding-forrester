37e1e9c64aed5eabd5d6ca0323bbf8b30a945585
HMisawa3 <mintia.jaxa@gmail.com>
2021-12-15 16:37:15 +0900

    no message

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,13 +5,10 @@
   <meta charset="utf-8">
   <title>FIND A BOOK</title>
   <meta name="description" content="">
-  @if(app('env')=='local')
-  <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-  <link rel="shortcut icon" href="{{ asset('favicon.ico') }}">
-  @else
+
   <link rel="stylesheet" href="{{ secure_asset('css/app.css') }}">
   <link rel="shortcut icon" href="{{ secure_asset('favicon.ico') }}">
-  @endif
+
 
   <meta name="viewport" content="width=device-width,initial-scale=1">
 </head>
