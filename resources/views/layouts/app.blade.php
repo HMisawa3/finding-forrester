783878fb7d447bb3fbcb25da9ef0d4dc6964cf03
HMisawa3 <mintia.jaxa@gmail.com>
2021-12-15 18:29:15 +0900

    no message

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,11 +5,8 @@
   <meta charset="utf-8">
   <title>FIND A BOOK</title>
   <meta name="description" content="">
-
   <link rel="stylesheet" href="{{ secure_asset('css/app.css') }}">
   <link rel="shortcut icon" href="{{ secure_asset('favicon.ico') }}">
-
-
   <meta name="viewport" content="width=device-width,initial-scale=1">
 </head>
 
