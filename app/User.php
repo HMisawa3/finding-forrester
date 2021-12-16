4b1abb43ae2f22774cabb4fbb00e35ee3bd8c73e
HMisawa3 <mintia.jaxa@gmail.com>
2021-12-16 14:44:05 +0900

    protected $redirectTo = '/book'; 新規登録したら/bookに遷移するように指定 RegistersUsers.phpの下記記述をコメントアウト、バリデーション が引っかかるため $this->validator($request->all())->validate();

--- app/User.php
+++ app/User.php
@@ -15,7 +15,9 @@ class User extends Authenticatable
      *
      * @var array
      */
-
+    protected $fillable = [
+        'name', 'email', 'password', 'address', 'tel', 'time', 'image'
+    ];
 
     /**
      * The attributes that should be hidden for arrays.
