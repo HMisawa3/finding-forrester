f89f07648f233985aa8313eac7b4a3fd2cfb1dfc
HMisawa3 <mintia.jaxa@gmail.com>
2021-12-17 13:09:59 +0900

    no message

--- resources/views/user/edit.blade.php
+++ resources/views/user/edit.blade.php
@@ -5,26 +5,26 @@
 <div id="index_wrap">
     <h1>ショップ情報編集</h1>
 
-    <form action="{{ route('shop.update')}}" method="post">
+    <form class="shop_edit_form" action="{{ route('shop.update')}}" method="post">
     @method('PUT')
     @csrf
     <label>店舗名</label>
-        <input type="text" name="name" value="{{ $shop->name }}">
+        <input class="shop_edit" type="text" name="name" value="{{ $shop->name }}">
     <label>メールアドレス</label>
-        <input type="email" name="email" value="{{ $shop->email }}">
+        <input class="shop_edit" type="email" name="email" value="{{ $shop->email }}">
     <label>パスワード</label>
-        <input type="password" name="password" value="{{ $shop->password }}" readonly="readonly">
+        <input class="shop_edit" type="password" name="password" value="{{ $shop->password }}" readonly="readonly">
     <label>新しいパスワード</label>
-        <input type="password" name="newPassword">
+        <input class="shop_edit" type="password" name="newPassword">
     <label>住所</label>
-        <input type="text" name="address" value="{{ $shop->address }}">
+        <input class="shop_edit" type="text" name="address" value="{{ $shop->address }}">
     <label>電話番号</label>
-        <input type="number" name="tel" value="{{ $shop->tel }}">
+        <input class="shop_edit" type="number" name="tel" value="{{ $shop->tel }}">
     <label>営業時間</label>
-        <input type="text" name="time" value="{{ $shop->time }}">
+        <input class="shop_edit" type="text" name="time" value="{{ $shop->time }}">
     <label>イメージ画像</label>
-        <input type="file" name="image" value="{{ $shop->image }}">
-        <input type="submit">
+        <input class="shop_edit" type="file" name="image" value="{{ $shop->image }}">
+        <input class="shop_edit_button" type="submit">
     </form>
 </div>
 
